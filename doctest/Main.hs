96ee95f0d5b8eec328d89e1bcb664c533e137f95
Yoo Chung <dev@chungyc.org>
2021-02-15 07:11:46 -0500

    Use fast mode with doctest.

--- doctest/Main.hs
+++ doctest/Main.hs
@@ -2,6 +2,7 @@ import           Test.DocTest
 
 main :: IO ()
 main = doctest $
+  "--fast" :
   map ("src/Problems/" ++) ["P01.hs", "P02.hs", "P03.hs", "P04.hs", "P05.hs",
                             "P06.hs", "P07.hs", "P08.hs", "P09.hs", "P10.hs",
                             "P11.hs", "P12.hs", "P13.hs", "P14.hs", "P15.hs"]
