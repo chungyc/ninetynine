08c7d9fef072a2c1e7737675787809305223a2e4
Yoo Chung <dev@chungyc.org>
2021-02-23 17:50:49 -0500

    Restrict to int labels for graph.  Define Graph type class.

--- src/Problems/Graphs.hs
+++ src/Problems/Graphs.hs
@@ -4,55 +4,60 @@ module Problems.Graphs (
   AdjacencyLists (AdjacencyLists),
   AdjacencyListMap (AdjacencyListMap),
   Friendly (Friendly),
-  Graph (vertexes, edges, neighbors, valid),
+  Graph (vertexes, edges, neighbors, isValid),
   ) where
 
-import           Data.Map.Lazy
+import qualified Data.IntMap.Strict as Map
 
 -- | A vertex, its contents, and its neighboring vertexes in a graph with direct adjacency lists.
 --
 -- Neighboring vertexes are included directly as values.
-data Value a = Value a [Value a]
+data Value = Value Int [Value]
   deriving (Eq, Show)
 
-v1 :: Value Int
+v1 :: Value
 v1 = Value 1 [v2, v4]
 
-v2 :: Value Int
+v2 :: Value
 v2 = Value 2 [v1, v3, v3]
 
-v3 :: Value Int
+v3 :: Value
 v3 = Value 3 [v2, v4]
 
-v4 :: Value Int
+v4 :: Value
 v4 = Value 4 [v1, v2, v3, v5]
 
-v5 :: Value Int
+v5 :: Value
 v5 = Value 5 [v4]
 
 -- | Standard definition of graph.  Set of vertexes and set of edges.
-data List a = List ([a], [(a, a)])
+data List = List ([Int], [(Int, Int)])
   deriving (Eq, Show)
 
 -- | Graph represented as with adjacency lists.
-data AdjacencyLists a = AdjacencyLists [(a, [a])]
+data AdjacencyLists = AdjacencyLists [(Int, [Int])]
   deriving (Eq, Show)
 
 -- | Adjacency lists in a map.
-data AdjacencyListMap a = AdjacencyListMap (Map a [a])
-  deriving (Eq, Show)
+newtype AdjacencyListMap = AdjacencyListMap (Map.IntMap [Int])
 
 -- | Human-friendly form.
 --
 -- Similar to DOT.
-data Friendly a = Friendly [[a]]
+data Friendly a = Friendly [[Int]]
   deriving (Eq, Show)
 
 class Graph g where
-  vertexes :: g -> [a]
-  edges :: g -> [(a,a)]
-  neighbors :: g -> a -> [a]
-  valid :: g -> Bool
+  vertexes :: g -> [Int]
+  edges :: g -> [(Int,Int)]
+  neighbors :: g -> Int -> [Int]
+  isValid :: g -> Bool
+
+instance Graph AdjacencyListMap where
+  vertexes (AdjacencyListMap m) = Map.keys m
+  edges (AdjacencyListMap m) = concat $ map (\(s, ds) -> [(s, d) | d <- ds]) (Map.toList m)
+  neighbors (AdjacencyListMap m) v = Map.findWithDefault [] v m
+  isValid (AdjacencyListMap m) = all (\ds -> all (flip Map.member $ m) ds) (Map.elems m)
 
 -- Labeled graphs. (Weighted graphs.)
 
