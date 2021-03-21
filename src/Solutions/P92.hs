dd3c42fd1f035506cd3897649f4c7f343416397f
Yoo Chung <dev@chungyc.org>
2021-03-20 23:16:37 -0400

    Tidy up implementation of problem 92.

--- src/Solutions/P92.hs
+++ src/Solutions/P92.hs
@@ -8,7 +8,7 @@ module Solutions.P92 (gracefulTree, gracefulTree') where
 import           Data.List       (permutations, sort, sortOn)
 import           Data.Map.Lazy   (Map, (!))
 import qualified Data.Map.Lazy   as Map
-import           Data.Maybe      (fromJust, isJust)
+import           Data.Maybe      (fromJust)
 import           Data.Set        (Set)
 import qualified Data.Set        as Set
 import           Problems.Graphs
@@ -36,10 +36,24 @@ It gives up and tries another if a partial labeling cannot be extended.
 gracefulTree :: G -> Maybe (Map Vertex Int)
 gracefulTree g
   | Set.null $ vertexes g = Just Map.empty
-  | isTree g              = expand g vs Map.empty (Set.fromList [1..size]) Set.empty
+  | isTree g              = expand p
   | otherwise             = undefined
   where vs = vertexesByDegree g
-        size = length vs
+        p = Partial { graph = g
+                    , labeling = Map.empty
+                    , remainingVertexes = vs
+                    , remainingVertexLabels = Set.fromList [1..(length vs)]
+                    , definiteEdgeDifferences = Set.empty
+                    }
+
+-- | A partial graceful labeling of a tree graph and associate state for building it up.
+data Partial = Partial
+  { graph                   :: G               -- ^ Tree to label gracefully
+  , labeling                :: Map Vertex Int  -- ^ Partial labeling of vertexes
+  , remainingVertexes       :: [Vertex]        -- ^ Vertexes remaining to be labeled
+  , remainingVertexLabels   :: Set Int         -- ^ Set of labels yet to be used with vertexes
+  , definiteEdgeDifferences :: Set Int         -- ^ Set of differences already determined for edges
+  }
 
 -- | Trying vertexes with higher degrees first might constrain labeling more,
 -- i.e, prune the search space more.
@@ -47,41 +61,37 @@ vertexesByDegree :: G -> [Vertex]
 vertexesByDegree g = reverse $ sortOn (\v -> Set.size $ neighbors v g) $ Set.toList $ vertexes g
 
 -- | Label one more vertex.
-expand :: G               -- ^ Tree to label gracefully
-       -> [Vertex]        -- ^ Vertexes remaining to be labeled
-       -> Map Vertex Int  -- ^ Partial labeling of vertexes
-       -> Set Int         -- ^ Set of labels yet to be used with vertexes
-       -> Set Int         -- ^ Set of differences already determined for edges
-       -> Maybe (Map Vertex Int)  -- ^ Graceful labeling of tree
-expand _ [] lbls _ _              = Just lbls
-expand g (v:vs) lbls vlbls ediffs = labelVertex g v vlbls vs lbls vlbls ediffs
+expand :: Partial -> Maybe (Map Vertex Int)
+expand (Partial { labeling = lbls, remainingVertexes = [] }) = Just lbls
+expand p = labelVertex p' v ls
+  where (v:vs) = remainingVertexes p
+        ls = remainingVertexLabels p
+        p' = p { remainingVertexes = vs }
 
 -- | Try labeling a particular vertex with the given label candidates.
-labelVertex :: G -> Vertex -> Set Int -> [Vertex] -> Map Vertex Int -> Set Int -> Set Int -> Maybe (Map Vertex Int)
-labelVertex g v candidates vs lbls vlbls ediffs
-  | Set.null candidates = Nothing
-  | disjoint            = try $ expand g vs lbls' vlbls' ediffs'
-  | otherwise           = next
-  where candidate = Set.findMin candidates
-        candidates' = Set.delete candidate candidates
-        maybeDiffs = edgeDiffs g v candidate lbls
-        diffs = fromJust maybeDiffs
-        disjoint = isJust maybeDiffs && Set.disjoint diffs ediffs
-        ediffs' = Set.union ediffs diffs
-        lbls' = Map.insert v candidate lbls
-        vlbls' = Set.delete candidate vlbls
-        try Nothing = next
-        try r       = r
-        next = labelVertex g v candidates' vs lbls vlbls ediffs
+labelVertex :: Partial -> Vertex -> Set Int -> Maybe (Map Vertex Int)
+labelVertex p v ls | Set.null ls = Nothing
+                   | disjoint    = maybe next Just $ expand p'
+                   | otherwise   = next
+  where l = Set.findMin ls  -- choose label to try arbitrarily
+        e = edgeDiffs p v l
+        es = definiteEdgeDifferences p
+        disjoint = maybe False (Set.disjoint es) e
+        next = labelVertex p v $ Set.delete l ls
+        p' = p { labeling = Map.insert v l $ labeling p
+               , remainingVertexLabels = Set.delete l $ remainingVertexLabels p
+               , definiteEdgeDifferences = Set.union es $ fromJust e
+               }
 
 -- | Gather the absolute differences between vertexes in the edge connected to the given vertex.
 -- The given vertex would not have been labeled, so this cannot be double-counted.
-edgeDiffs :: G -> Vertex -> Int -> Map Vertex Int -> Maybe (Set Int)
-edgeDiffs g v lbl lbls
-  | Set.size diffs == Set.size labeledNeighbors = Just diffs
-  | otherwise                                   = Nothing
-  where labeledNeighbors = Set.filter (\v' -> Map.member v' $ lbls) $ neighbors v g
-        diffs = Set.map (\v' -> abs $ lbl - lbls ! v') labeledNeighbors
+edgeDiffs :: Partial -> Vertex -> Int -> Maybe (Set Int)
+edgeDiffs p v l | bijected  = Just diffs
+                | otherwise = Nothing
+  where ls = labeling p
+        vs = Set.filter (\v' -> Map.member v' ls) $ neighbors v $ graph p
+        diffs = Set.map (\v' -> abs $ l - ls ! v') vs
+        bijected = Set.size diffs == Set.size vs
 
 isTree :: G -> Bool
 isTree g = all (\(v,v') -> length (paths v v' g) == 1) [ (v,v') | v <- vs, v' <- vs, v < v' ]
