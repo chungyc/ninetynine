8d388c81b25e379bc2e36cc022f5c04f8f122dab
Yoo Chung <dev@chungyc.org>
2021-04-04 01:44:48 -0400

    Add explanations to solution for problem 89.

--- src/Solutions/P89.hs
+++ src/Solutions/P89.hs
@@ -18,7 +18,9 @@ bipartite g = partition g (vertexes g, Set.empty, Set.empty, Set.empty)
 
 partition :: G -> (Set Vertex, Set Vertex, Set Vertex, Set Vertex) -> Bool
 partition g (remaining, boundary, us, vs)
+  -- Went through all vertexes without encountering a contradiction with bipartiteness.
   | Set.null boundary && Set.null remaining = True
+  -- Start again with disconnected component.
   | Set.null boundary = let (v', r') = Set.deleteFindMin remaining
                         in partition g (r', Set.singleton v', us, vs)
   -- If bipartite, then boundary' must necessarily end up the same set which will include vs.
