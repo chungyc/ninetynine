382588750fd353139be60e38f2dfc627c7e958bd
Yoo Chung <dev@chungyc.org>
2023-01-01 12:54:56 -0500

    Stop using incomplete-uni-patterns in P42Spec.

--- test/Problems/P42Spec.hs
+++ test/Problems/P42Spec.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
-
 {-|
 Copyright: Copyright (C) 2021 Yoo Chung
 License: GPL-3.0-or-later
@@ -7,6 +5,7 @@ Maintainer: dev@chungyc.org
 -}
 module Problems.P42Spec (spec) where
 
+import           Data.Maybe            (fromJust)
 import           Problems.P33
 import qualified Problems.P42          as Problem
 import qualified Solutions.P42         as Solution
@@ -18,7 +17,7 @@ properties :: (Integer -> Integer -> Maybe Integer) -> String -> Spec
 properties multiplicativeInverse name = describe name $ do
   prop "is multiplicative inverse" $
     \(Positive a) -> \(Positive n) -> n > 1 ==> coprime a n ==>
-    multiplicativeInverse a n `shouldSatisfy` \(Just x) -> a * x `mod` n == 1
+    multiplicativeInverse a n `shouldSatisfy` \x -> a * (fromJust x) `mod` n == 1
 
   prop "has no multiplicative inverse when not coprime" $
     \(Positive a) -> \(Positive n) -> n > 1 ==> not (coprime a n) ==>
