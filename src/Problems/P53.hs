e7d2f313118784be25301a0385f598b0ac34eba4
Yoo Chung <dev@chungyc.org>
2023-01-06 14:17:25 -0500

    Restrict exports.

--- src/Problems/P53.hs
+++ src/Problems/P53.hs
@@ -6,7 +6,7 @@ Maintainer: dev@chungyc.org
 
 Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.P53".
 -}
-module Problems.P53 where
+module Problems.P53 (isTheorem) where
 
 import           Problems.Logic (Formula (..))
 import qualified Solutions.P53  as Solution
