57087c76aef049c55a10fdb0e45ec5f787502743
Yoo Chung <dev@chungyc.org>
2023-01-12 19:19:53 -0500

    Improved property-based testing in problem 81.

--- test/Problems/P81Spec.hs
+++ test/Problems/P81Spec.hs
@@ -1,11 +1,10 @@
 {-|
-Copyright: Copyright (C) 2021 Yoo Chung
+Copyright: Copyright (C) 2023 Yoo Chung
 License: GPL-3.0-or-later
 Maintainer: dev@chungyc.org
 -}
 module Problems.P81Spec (spec) where
 
-import           Data.List                 (nub)
 import           Data.Maybe                (fromJust)
 import qualified Data.Set                  as Set
 import           Problems.Graphs
@@ -18,45 +17,40 @@ import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 
 properties :: (Vertex -> Vertex -> G -> [[Vertex]]) -> String -> Spec
-properties paths name =
-  describe name $ modifyMaxSize (const 25) $ do
-    prop "includes path" $ do
-      \(Sets (vs, es)) -> do
-        vs' <- sublistOf $ Set.toList vs
-        return $ forAll (shuffle vs') $ \trail ->
-          length trail > 1 ==>
-          head trail /= last trail ==>
-          let path = nub trail
-              a = head path
-              b = last path
-              es' = Set.union es $ pathEdges path  -- ensure path exists
-          in paths a b (fromJust $ toGraph (vs, es')) `shouldSatisfy` elem path
+properties paths name = describe name $ modifyMaxSize (const 25) $ do
+  prop "includes path" $ \(Sets (vs, es)) ->
+    forAll (sublistOf $ Set.toList vs) $ \vs' ->
+    forAll (shuffle vs') $ \path ->
+    not (null path) ==>
+    classify (length path <= 2) "trivial" $
+    let src = head path
+        dst = last path
+        es' = Set.union es $ pathEdges path  -- ensure path exists
+        g = fromJust $ toGraph (vs, es')
+    in paths src dst g `shouldSatisfy` elem path
 
-    prop "does not include non-existant path" $ do
-      \(Sets (vs, es)) -> do
-        vs' <- sublistOf $ Set.toList vs
-        return $ forAll (shuffle vs') $ \trail ->
-          length trail > 1 ==>
-          head trail /= last trail ==>
-          let path = nub trail
-              a = head path
-              b = last path
-              es' = Set.difference es $ pathEdges path  -- ensure path does not exist
-          in paths a b (fromJust $ toGraph (vs, es')) `shouldNotSatisfy` elem path
+  prop "does not include non-existant path" $ \(Sets (vs, es)) ->
+    forAll (sublistOf $ Set.toList vs) $ \vs' ->
+    forAll (shuffle vs') $ \path ->
+    length path > 1 ==>
+    let src = head path
+        dst = last path
+        es' = Set.difference es $ pathEdges path  -- ensure path does not exist
+        g = fromJust $ toGraph (vs, es')
+    in paths src dst g `shouldNotSatisfy` elem path
 
-  where pathEdges []             = Set.empty
-        pathEdges [_]            = Set.empty
-        pathEdges (u : vs@(v:_)) = Set.insert (Edge (u, v)) $ pathEdges vs
+  where
+    -- The set of edges forming the given path.
+    pathEdges path = Set.fromList $ map Edge $ zip path (tail path)
 
 examples :: Spec
-examples = do
-  describe "Examples" $ do
-    it "paths 1 4 $ toG $ Paths [[1,2,3], [1,3,4,2], [5,6]]" $ do
-      paths 1 4 (toG $ Paths [[1,2,3], [1,3,4,2], [5,6]])
-        `shouldMatchList` [[1,2,4], [1,2,3,4], [1,3,2,4], [1,3,4]]
+examples = describe "Examples" $ do
+  it "paths 1 4 $ toG $ Paths [[1,2,3], [1,3,4,2], [5,6]]" $ do
+    paths 1 4 (toG $ Paths [[1,2,3], [1,3,4,2], [5,6]])
+      `shouldMatchList` [[1,2,4], [1,2,3,4], [1,3,2,4], [1,3,4]]
 
-    it "paths 2 6 $ toG $ Paths [[1,2,3], [1,3,4,2], [5,6]]" $ do
-      paths 2 6 (toG $ Paths [[1,2,3], [1,3,4,2], [5,6]]) `shouldBe` []
+  it "paths 2 6 $ toG $ Paths [[1,2,3], [1,3,4,2], [5,6]]" $ do
+    paths 2 6 (toG $ Paths [[1,2,3], [1,3,4,2], [5,6]]) `shouldBe` []
 
   where paths = Problem.paths
 
