ab9f96853bc383ba3bfd545dfc34c758e6c45325
Yoo Chung <dev@chungyc.org>
2023-01-17 00:01:36 -0500

    hlint on benchmarks for problems 61-69.

--- bench/Problems/P63Bench.hs
+++ bench/Problems/P63Bench.hs
@@ -12,10 +12,10 @@ import qualified Solutions.P63 as Solution
 group :: Benchmark
 group = bgroup "P63"
   [ bench "completeBinaryTree 30000" $ nf Problem.completeBinaryTree 30000
-  , bench "isCompleteBinaryTree with tree size 30000" $ nf Problem.isCompleteBinaryTree $ tree30000
+  , bench "isCompleteBinaryTree with tree size 30000" $ nf Problem.isCompleteBinaryTree tree30000
   , bgroup "Solutions"
     [ bench "completeBinaryTree 30000" $ nf Solution.completeBinaryTree 30000
-    , bench "isCompleteBinaryTree with tree size 30000" $ nf Solution.isCompleteBinaryTree $ tree30000
+    , bench "isCompleteBinaryTree with tree size 30000" $ nf Solution.isCompleteBinaryTree tree30000
     ]
   ]
   where tree30000 = Solution.completeBinaryTree 30000
