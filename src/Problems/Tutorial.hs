77b216478a61052c9a82061aa8482a191bc72c67
Yoo Chung <dev@chungyc.org>
2023-01-17 23:34:10 -0500

    Starting work on a tutorial problem.

---
+++ src/Problems/Tutorial.hs
@@ -0,0 +1,9 @@
+{- |
+Description: Tutorial for solving problems
+Copyright: Copyright (C) 2023 Yoo Chung
+License: GPL-3.0-or-later
+Maintainer: dev@chungyc.org
+
+Part of Ninety-Nine Haskell "Problems".  Some solutions are in "Solutions.Tutorial".
+-}
+module Problems.Tutorial where
---
+++ src/Solutions/Tutorial.hs
@@ -0,0 +1,9 @@
+{- |
+Description: Tutorial for solving problems
+Copyright: Copyright (C) 2023 Yoo Chung
+License: GPL-3.0-or-later
+Maintainer: dev@chungyc.org
+
+Some solutions to "Problems.Tutorial" of Ninety-Nine Haskell "Problems".
+-}
+module Solutions.Tutorial where
