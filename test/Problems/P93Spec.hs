9b6e04cdae1c6ab183fb2313b6789139a9f79e69
Yoo Chung <dev@chungyc.org>
2023-01-22 01:10:44 -0500

    Use Parsec instead of a manually crafted parser to parse equations in problem 93.

--- test/Problems/P93Spec.hs
+++ test/Problems/P93Spec.hs
@@ -13,6 +13,7 @@ import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 import           Text.Parsec
+import           Text.Parsec.Expr
 import           Text.Parsec.Language  (emptyDef)
 import qualified Text.Parsec.Token     as Token
 
@@ -73,13 +74,6 @@ extractNumbers s | Right l <- parse numbers "" s = catMaybes l
   where numbers = many $ (Just <$> natural) <|> (anyChar >> return Nothing)
         natural = Token.natural $ Token.makeTokenParser emptyDef
 
-{-
-extractNumbers s = extract $ tokenize s
-  where extract []           = []
-        extract ((N n) : ps) = n : extract ps
-        extract (_:ps)       = extract ps
--}
-
 -- | An equation in parsed form.
 data Expr = Number Integer
           | Add Expr Expr
@@ -91,55 +85,28 @@ data Expr = Number Integer
 
 -- | Parse an equation from a string.
 parseExpr :: String -> Expr
-parseExpr s
-  | ([], [Ex e]) <- parseExpr' (tokenize s, []) = e
-  | otherwise = error $ "parse error: " ++ s
+parseExpr s | Right e <- p = e
+            | Left err <- p = error $ show err
 
--- | Elements on the stack for parsing the context-free grammar.
-data ParseElement = T Token | Ex Expr deriving Show
+  where p = parse expr "" s
 
--- | Manually parse the context-free grammar.
-parseExpr' :: ([Token], [ParseElement]) -> ([Token], [ParseElement])
-parseExpr' (ts, T (N n) : ps)                 = parseExpr' (ts, Ex (Number n) : ps)
-parseExpr' (M:ts, ps@(Ex _ : T P : Ex _ : _)) = parseExpr' (ts, T M : ps)
-parseExpr' (D:ts, ps@(Ex _ : T P : Ex _ : _)) = parseExpr' (ts, T D : ps)
-parseExpr' (ts, Ex v : T P : Ex u : ps)       = parseExpr' (ts, Ex (Add u v) : ps)
-parseExpr' (M:ts, ps@(Ex _ : T S : Ex _ : _)) = parseExpr' (ts, T M : ps)
-parseExpr' (D:ts, ps@(Ex _ : T S : Ex _ : _)) = parseExpr' (ts, T D : ps)
-parseExpr' (ts, Ex v : T S : Ex u : ps)       = parseExpr' (ts, Ex (Subtract u v) : ps)
-parseExpr' (ts, Ex v : T M : Ex u : ps)       = parseExpr' (ts, Ex (Multiply u v) : ps)
-parseExpr' (ts, Ex v : T D : Ex u : ps)       = parseExpr' (ts, Ex (Divide u v) : ps)
-parseExpr' (ts, T PE : Ex e : T PS : ps)      = parseExpr' (ts, Ex e : ps)
-parseExpr' (t:ts, ps)                         = parseExpr' (ts, T t : ps)
-parseExpr' ([], [Ex v, T E, Ex u])            = ([], [Ex (Equals u v)])
-parseExpr' _                                  = undefined
+        expr = buildExpressionParser table term
+        term = parens expr <|> number
+        number = Number <$> natural
 
--- | Lexical tokens.
-data Token = N Integer | P | S | M | D | E | PS | PE
-  deriving (Eq, Show)
+        table = [ [ Infix (op "*" >> return Multiply) AssocLeft
+                  , Infix (op "/" >> return Divide) AssocLeft
+                  ]
+                , [ Infix (op "+" >> return Add) AssocLeft
+                  , Infix (op "-" >> return Subtract) AssocLeft
+                  ]
+                , [ Infix (op "=" >> return Equals) AssocNone ]
+                ]
 
--- | Parse a string into its lexical tokens.
-tokenize :: String -> [Token]
-tokenize s = reverse $ snd $ tokenize' (s, [])
-
-tokenize' :: (String, [Token]) -> (String, [Token])
-tokenize' ("",ts)     = ("", ts)
-tokenize' (' ':xs,ts) = tokenize' (xs, ts)
-tokenize' ('+':xs,ts) = tokenize' (xs, P:ts)
-tokenize' ('-':xs,ts) = tokenize' (xs, S:ts)
-tokenize' ('*':xs,ts) = tokenize' (xs, M:ts)
-tokenize' ('/':xs,ts) = tokenize' (xs, D:ts)
-tokenize' ('=':xs,ts) = tokenize' (xs, E:ts)
-tokenize' ('(':xs,ts) = tokenize' (xs, PS:ts)
-tokenize' (')':xs,ts) = tokenize' (xs, PE:ts)
-tokenize' (xs,ts)     = tokenize' (xs', N n : ts)
-  where (n, xs') = parseNumber xs
-
--- | Parse a string into a number and the rest of the string.
-parseNumber :: String -> (Integer, String)
-parseNumber s = (n, s')
-  where n = read $ takeWhile (`elem` ['0'..'9']) s
-        s' = dropWhile (`elem` ['0'..'9']) s
+        parens = Token.parens lexer
+        op = Token.reservedOp lexer
+        natural = Token.natural lexer
+        lexer = Token.makeTokenParser emptyDef
 
 evalEquation :: Expr -> Bool
 evalEquation (Equals x y) = evalExpr x == evalExpr y
